2fe7a9257d3f870e309e06f756188eb99af93c18
carterawebb <carterwebb@nevada.unr.edu>
2023-04-18 16:20:55 -0700

    Base File - Names, Assignment, Date, Class

---
+++ cpe301FinalProject/cpe301FinalProject.ino
@@ -0,0 +1,17 @@
+/****************************************
+ * Names: Carter Webb, Gavin Casper,
+          Ryan Du, Tyler Sar
+ * Assignment: Final Project
+ * Date: 04/18/2023
+ * Class: CPE 301 Spring 2023
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
