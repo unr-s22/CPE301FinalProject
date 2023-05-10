0594670a9277697716b050a606122e7d01625381
1106-Sar-Tyler <98282688+1106-Sar-Tyler@users.noreply.github.com>
2023-05-09 20:24:35 -0700

    Update Water_Senor.ino
    Fixed errors, removed pinMode/DigitalWrite, etc. This is the most updated version of the water level sensor. Added the correct definitions, and included a water level address.

--- Water_Senor.ino
+++ Water_Senor.ino
@@ -1,12 +1,18 @@
-/* ISR/Water Sensor
-*  Name: Tyler Sar
-*  Class: CPE 301 Spring '23
-*/
+// Water level Senor
+// By Tyler Sar
+// 5/1/2023
 
-// Digital port b registers
-volatile unsigned char* port_b = (unsigned char*) 0x25;
-volatile unsigned char* ddr_b = (unsigned char*) 0x24;
-volatile unsigned char* pin_b = (unsigned char*) 0x23;
+// Definition
+
+#define RDA 0x80  // Receive Data Avaliable
+#define TBE 0x20  // Transfer Buffer Empty
+#define SENSOR_PIN 1 // Analog Pin 1
+#define MIN_WATER_LEVEL 70.0 // The water level
+
+// Water Level Senor Address
+volatile unsigned char *port_f = (unsigned char*) 0x31;
+volatile unsigned char *ddr_f = (unsigned char*) 0x30;
+volatile unsigned char *pin_f = (unsigned char*) 0x2F;
 
 // Analog
 volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
@@ -19,118 +25,83 @@ volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
-#define RDA 0x80
-#define TBE 0x20
 
-// Define the pin
-#define SENSOR_PIN A1
 
-//Minimum water level in percent
-#define MIN_WATER_LEVEL 10
+
 
 void setup() {
-
   Serial.begin(9600);
   // Initialize the ADC
   adc_init();
-
 }
 
 void loop() {
 
+  // Read the voltage from the sensor
   float voltage = readVoltage(SENSOR_PIN);
- 
-  // Calculate the water level based on the voltage reading
-  float water_level = (voltage - 0.5) * 100;
-  Serial.println(water_level);  
-  // check to see if water is below min level
-  if (water_level < MIN_WATER_LEVEL){
 
-    // Prints a message
-    Serial.println("Water is too low");
+  // calculate the level based on the reading
+  float waterLevel = (voltage - 0.5) * 100;
+  // testing the readings
+  Serial.println(waterLevel);
 
+  // check water level
+  if (waterLevel < MIN_WATER_LEVEL){
+      // display this message when water is low
+      Serial.println("Water is too low");
   }
 
   delay(1000);
 }
+  // read from the analog pin
+  float readVoltage(int pinIn){
+    *ddr_f &= 0b11111101;
+    *port_f |= 0b00000010;
 
-float readVoltage(int pin){
+   // set the pin as input and read the voltage
+    float voltage = adc_read(pinIn) * 5.0 / 1023.0;
 
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
+    return voltage;
+  }
 
-  // set the pin as input and read the voltage
-  pinMode(pin, INPUT_PULLUP);
-  float voltage = analogRead(pin) * 5.0 / 1023.0;
+  void U0Init(int U0baud) {
+  unsigned long FCPU = 16000000;
+  unsigned int tbaud;
+  tbaud = (FCPU / 16 / U0baud - 1);
 
-  return voltage;
+  *myUCSR0A = 0x20;
+  *myUCSR0B = 0x18;
+  *myUCSR0C = 0x06;
+  *myUBRR0  = tbaud;
 }
 
+void U0putChar(unsigned char U0pdata) {
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
+}
 
-
-// Initialize the ADC
 void adc_init() {
-    // setup the A register
-    *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
-    *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
-    *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC interrupt
-    *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to set prescaler selection to slow reading
-    // setup the B register
-    *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
-    *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
-    // setup the MUX Register
-    *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
-    *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
-    *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
-    *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
-    *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADCSRA |= 0b10000000;
+  *my_ADCSRA &= 0b11011111;
+  *my_ADCSRA &= 0b11110111;
+  *my_ADCSRA &= 0b11111000;
+  *my_ADCSRB &= 0b11110111;
+  *my_ADCSRB &= 0b11111000;
+  *my_ADMUX  &= 0b01111111;
+  *my_ADMUX  |= 0b01000000;
+  *my_ADMUX  &= 0b11011111;
+  *my_ADMUX  &= 0b11100000;
 }
+
 unsigned int adc_read(unsigned char adc_channel_num) {
-    // clear the channel selection bits (MUX 4:0)
-    *my_ADMUX  &= 0b11100000;
-    // clear the channel selection bits (MUX 5)
-    *my_ADCSRB &= 0b11110111;
-    // set the channel number
-    if(adc_channel_num > 7)
-    {
-    // set the channel selection bits, but remove the most significant bit (bit 3)
+  *my_ADMUX  &= 0b11100000;
+  *my_ADCSRB &= 0b11110111;
+  if(adc_channel_num > 7) {
     adc_channel_num -= 8;
-    // set MUX bit 5
-    *my_ADCSRB |= 0b00100000;
-    }
-    // set the channel selection bits
-    *my_ADMUX  += adc_channel_num;
-    // set bit 6 of ADCSRA to 1 to start a conversion
-    *my_ADCSRA |= 0x40;
-    // wait for the conversion to complete
-    while((*my_ADCSRA & 0x40) != 0);
-    // return the result in the ADC data register
-    return *my_ADC_DATA;
+    *my_ADCSRB |= 0b00001000;
+  }
+  *my_ADMUX  += adc_channel_num;
+  *my_ADCSRA |= 0x40;
+  while((*my_ADCSRA & 0x40) != 0);
+  return *my_ADC_DATA;
 }
-
-void U0init(int U0baud) {
-    unsigned long FCPU = 16000000;
-    unsigned int tbaud;
-    tbaud = (FCPU / 16 / U0baud - 1);
-
-    // Same as (FCPU / (16 * U0baud)) - 1;
-    *myUCSR0A = 0x20;
-    *myUCSR0B = 0x18;
-    *myUCSR0C = 0x06;
-    *myUBRR0  = tbaud;
-}
-
-void U0putChar(unsigned char U0pdata)
-{
-    while((*myUCSR0A & TBE)==0);
-    *myUDR0 = U0pdata;
-}
-
-//
-//  INTERRUPTS
-//
-
-//void ISR () {
-
-//}
-
